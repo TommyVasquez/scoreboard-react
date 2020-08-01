f241196e722ccf0389865d6df22ad484d660a40c
Tommy Vasquez <tommyvasquezr@gmail.com>
2020-08-01 10:19:55 -0500

    bind event handlers to components

--- app.js
+++ app.js
@@ -60,18 +60,29 @@ class Counter extends React.Component {
         score: 0,
     };
 
-    incrementScore() {
+    incrementScore = () => {
         // console.log('Hi, from inside incrementScore');
         this.setState({
             score: this.state.score + 1,
         });
-    }
+    };
+
+    decrementScore = () => {
+        this.setState({
+            score: this.state.score - 1,
+        });
+    };
 
     render() {
         return (
             <div className="counter">
-                <button className="counter-action decrement"> - </button>
+                <button className="counter-action decrement" onClick={this.decrementScore}>
+                    {' '}
+                    -{' '}
+                </button>
                 <span className="counter-score">{this.state.score}</span>
+                {/**onclick={ () => this.incrementScore() } */}
+                {/**onClick={ this.incrementScore.bind(this) } */}
                 <button className="counter-action increment" onClick={this.incrementScore}>
                     {' '}
                     +{' '}
